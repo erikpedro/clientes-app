962ae9d428edec8caf145ad3f2118a4ab4855632
Erik Pedro <erikpedro@gmail.com>
2021-06-14 19:09:54 -0300

    Atualizando clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -19,10 +19,21 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8081/api/clientes', cliente);
   }
 
+
+  atualizar(cliente: Cliente) : Observable<any> {
+    return this.http.put<Cliente>(`http://localhost:8081/api/clientes/${cliente.id}`, cliente);
+  }
+
  
   getClientes() : Observable<Cliente[]> {
     return this.http.get<Cliente[]>('http://localhost:8081/api/clientes');
   }
+
+
+  getClienteById(id: number) : Observable<Cliente> {
+    return this.http.get<any>(`http://localhost:8081/api/clientes/${id}`);
+  }
+
 
 
 
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClientesService } from 'src/app/clientes.service';
 import {Cliente} from '../cliente'
 
@@ -12,15 +14,54 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: string[];
+  id: number;
 
-  constructor(private service : ClientesService) { 
+  constructor(
+    private service : ClientesService,
+    private router : Router,
+    private activatedRoute : ActivatedRoute) { 
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+ 
+    let params : Observable<Params> = this.activatedRoute.params
+    params.subscribe( urlParams => {
+      this.id = urlParams['id'];
+      if(this.id){
+
+      
+      this.service
+      .getClienteById(this.id)
+      .subscribe(
+        response => this.cliente = response, 
+        errorResponse => this.cliente = new Cliente()
+      )
+      }
+    } )
+    
+   
+  }
+
+  voltarParaListagem(){
+    this.router.navigate(['/clientes-lista'])
   }
 
   onSubmit(){
+    if(this.id){
+      this.service
+      .atualizar(this.cliente)
+      .subscribe(response => {
+        this.success = true;
+        this.errors = [];
+      }, errorResponse => {
+        this.errors = ['Erro ao Atualizar o Cliente.']
+      } )
+  
+      
+    }else{
+
+    
    this.service
    .salvar(this.cliente)
    .subscribe( response => {
@@ -33,6 +74,7 @@ export class ClientesFormComponent implements OnInit {
        this.errors =  errorResponse.error.errors;
    }
    )
+  }
 
   }
 
